ce0569677a8d0bffec44c9eb464941f351ddce73
qchau97 <vtquynhchau@gmail.com>
2021-03-13 23:01:16 +0700

    Setting up Google Cloud Platform Credentials & Getting the User Location

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, Button, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, Button, Alert, PermissionsAndroid } from 'react-native';
 import { Colors } from '../constants/Colors';
+import { } from 'react-native-maps';
+import RNLocation from 'react-native-location';
 
 const LocationSelector = () => {
-  const handleGetLocation = () => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [pickedLocation, setPickedLocation] = useState(null);
 
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Allow FitGoalApp to use your location?",
+          message:
+            "FitGoalApp uses this to provide more relevant and personalized experiences.",
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'Allow'
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera");
+        return true;
+      } else {
+        console.log("Camera permission denied");
+        return false;
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const handleGetLocation = async () => {
+    setIsFetching(true);
+    const hasPermission = await requestLocationPermission();
+    if (!hasPermission) return;
+    RNLocation.configure({ distanceFilter: null });
+    const currentLocation = await RNLocation.getLatestLocation({ timeout: 60000 });
+    setPickedLocation({
+      lat: currentLocation.latitude,
+      lng: currentLocation.longitude,
+    });
+    if (currentLocation) setIsFetching(false);
   };
 
   return (
     <View style={styles.locationSelector}>
       <View style={styles.mapPreview}>
-        <Text>No location chosen yet!</Text>
+        {isFetching ? <ActivityIndicator size='large' color={Colors.primary} /> : <Text>No location chosen yet!</Text>}
       </View>
       <Button title='Get Your Location' color={Colors.primary} onPress={handleGetLocation} />
     </View>
@@ -27,6 +66,8 @@ const styles = StyleSheet.create({
     height: 150,
     borderColor: '#ccc',
     borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
